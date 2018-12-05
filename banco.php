3693efd0a0dc4362aacaa9c77d4a6bf41c877663
Claidson <cchaidrich@hotmail.om>
2018-12-05 09:49:15 -0200

    banco

---
+++ banco.php
@@ -0,0 +1,21 @@
+<?php
+print "<h2>BANCO NO POSTGRESQL</h2>";
+$bdcon = pg_connect("dbname=pareador");
+//conecta a um banco de dados chamado "pareador"
+
+$con_string = "host=localhost port=5432 dbname=pareador user=postgres password=postgres";
+if(!$dbcon = pg_connect($con_string)) die ("Erro ao conectar ao banco<br>".pg_last_error($dbcon));
+//coneta a um banco de dados chamado "cliente" na máquina "localhost" com um usuário e senha
+print "<h2>CONSULTA SQL)</h2>";
+$result = pg_query($dbcon, "SELECT * FROM eventos");
+if (!$result) {
+  echo "Erro na consulta.<br>";
+  exit;
+}
+print "<h2>INSERT,DELETE E UPDATE</h2>";
+$insert = pg_query($dbcon, "INSERT INTO eventos (nome,local, data) VALUES (Peras, st_makepoint(-50.337571,-27.805085), '2018-03-25')");
+while ($row = pg_fetch_row($result)) {
+  echo "Nome $row[0]  Atividade: $row[1] Local: $row[2]";
+  echo "<br />\n";
+}
+?>
