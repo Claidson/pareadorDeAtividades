732f0570269f7e866aac633acb8aa8fe349c51ef
Claidson <cchaidrich@hotmaiil.com>
2018-12-08 00:05:39 -0200

    Salvando na index

---
+++ cadastrar.php
@@ -0,0 +1,22 @@
+<?php
+
+	$nome = $_POST['nome'];
+    $atividade = $_POST['atividade'];
+    $local = $_POST['local'];
+    $data_evento = $_POST['data_evento'];
+    $coordenada = "-50.337571,-27.805085";
+    //Criar a conexao
+    
+    $bdcon = pg_connect("dbname=pareador");
+//conecta a um banco de dados chamado "pareador"
+
+    $con_string = "host=localhost port=5432 dbname=pareador user=postgres password=postgres";
+    if(!$dbcon = pg_connect($con_string)) die ("Erro ao conectar ao banco<br>".pg_last_error($dbcon));
+    $query = "INSERT INTO eventos (nome,atividade, local, data_evento) VALUES ('$nome','$atividade', st_makepoint($local), '$data_evento')";
+    $insert = pg_query($dbcon, $query);
+    
+//voltar
+    $var = "<script>javascript:history.back(-2)</script>";
+     echo $var;
+
+?>
